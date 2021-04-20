13f47e3969c67ed1bae6ccb031ecee97d3c59c92
horyu1234 <horyu1234@naver.com>
2021-04-20 14:08:08 +0900

    Redux 개발 확장 프로그램 지원 추가

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import App from './App';
 import {createStore} from "redux"; // 추가
 import {Provider} from "react-redux"; // 추가
 import rootReducer from "./modules"; // 추가
+import {composeWithDevTools} from "redux-devtools-extension";
 
-const store = createStore(rootReducer); // 추가
+const store = createStore(rootReducer, composeWithDevTools()); // 추가
 
 ReactDOM.render(
     <React.StrictMode>
