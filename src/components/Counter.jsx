6b213dde339c20e69979cf784fd4ed98fb802547
horyu1234 <horyu1234@naver.com>
2021-04-20 14:27:44 +0900

    스타일 적용

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "./counter.css";
 
 const Counter = ({number, onIncrease, onDecrease}) => {
     return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onIncrease}>+ 1</button>
-            <button onClick={onDecrease}>- 1</button>
+        <div className="counter">
+            <h1 className="counter-text">{number}</h1>
+
+            <button className="counter-btn" onClick={onDecrease}>- 1</button>
+            <button className="counter-btn" onClick={onIncrease}>+ 1</button>
         </div>
     );
 };
