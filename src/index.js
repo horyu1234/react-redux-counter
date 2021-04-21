ed59175276a6f35fde2d1cc387fd00c8a9cc1925
horyu1234 <horyu1234@naver.com>
2021-04-22 00:41:43 +0900

    store 를 별도 파일로 분리

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from "redux"; // 추가
-import {Provider} from "react-redux"; // 추가
-import rootReducer from "./modules"; // 추가
-import {composeWithDevTools} from "redux-devtools-extension";
-
-const store = createStore(rootReducer, composeWithDevTools()); // 추가
+import {Provider} from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
     <React.StrictMode>
